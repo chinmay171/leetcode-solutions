d103898a34b45edcfed2cc390a8372bce3f94dc4
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-06-26 23:08:47 +0530

    Time: 18 ms (66.17%), Space: 55.4 MB (73.48%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maxOfPiles(int[] piles){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<piles.length; ++i){
+            max = Math.max(max, piles[i]);
+        }
+        return max;
+    }
+    
+    public boolean isPossible(int[] piles, int h, int speed){
+        int currHours = 0;
+        for(int banana : piles){
+            currHours += (double)banana/speed;
+            if(banana%speed != 0) currHours++;
+        }
+        if(currHours <= h)return true;
+        return false;
+    }
+    
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = maxOfPiles(piles);
+        int ans = high;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(isPossible(piles,h,mid) == true){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    public int smallestDivisor(int[] nums, int threshold) {
+        return minEatingSpeed(nums, threshold);
+    }
+}
