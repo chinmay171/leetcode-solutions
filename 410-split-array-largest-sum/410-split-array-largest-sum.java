22f6bfb2e7d5874f70f887aaba92220f73878511
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-06-26 20:19:39 +0530

    Time: 1 ms (85.09%), Space: 42.5 MB (13.07%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maxOfArray(int[] books, int N){
+        int max = 0;
+        for(int i = 0; i<N; ++i){
+            max = Math.max(max, books[i]);
+        }
+        return max;
+    }
+    
+    public int sumOfArray(int[] books, int N){
+        int sum = 0;
+        for(int i = 0; i<N; ++i){
+            sum += books[i];
+        }
+        return sum;
+    }
+    
+    public boolean isPossible(int[] pages, int maxLoads, int books, int totalStudents){
+        int currStudents = 1;
+        int currPages = 0;
+        for(int i = 0;currStudents<= totalStudents && i<books; i++){
+            if(currPages+pages[i] <= maxLoads){
+                currPages += pages[i];
+            }else{
+                currStudents++;
+                currPages = pages[i];
+            }
+        }
+        if(currStudents > totalStudents) return false;
+        return true;
+    }
+    
+    public int findPages(int[]A,int N,int M)
+    {
+        //Your code here
+        int left = maxOfArray(A, N);
+        int right = sumOfArray(A, N);
+        int ans = right;
+        
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            
+            if(isPossible(A,mid,N,M) == true){
+                ans = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return ans;
+    }
+    public int splitArray(int[] nums, int m) {
+        return findPages(nums, nums.length, m);
+    }
+}
