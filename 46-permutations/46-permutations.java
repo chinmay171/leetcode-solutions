67c8875834d90c57138e07df6dc2bea905260aed
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-03 13:03:29 +0530

    Time: 1 ms (97.91%), Space: 45.1 MB (18.40%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -2,28 +2,38 @@ class Solution {
     List<List<Integer>> res;
     int[] nums;
     
-    public void permutation(int currBox, boolean[] itemPlaced, List<Integer> boxes){
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void permutation(int currBox, List<Integer> boxes){
         if(currBox == nums.length){
-            List<Integer> temp = new ArrayList<>(boxes);
+            List<Integer> temp = new ArrayList<>();
+            for(int i = 0; i<nums.length;++i){
+                temp.add(nums[i]);
+            }
             res.add(temp);
             return;
         }
         
-        for(int i = 0; i<nums.length;++i){
-            if(itemPlaced[i] == false){
-                itemPlaced[i] = true;
-                boxes.add(nums[i]);
-                permutation(currBox+1, itemPlaced, boxes);
-                boxes.remove(boxes.size()-1);
-                itemPlaced[i] = false;
-            }
+        for(int i = currBox; i<nums.length;++i){
+            // if(itemPlaced[i] == false){
+            //     itemPlaced[i] = true;
+                // boxes.add(nums[i]);
+                swap(nums, currBox, i);
+                permutation(currBox+1, boxes);
+                // boxes.remove(boxes.size()-1);
+                swap(nums, currBox,i);
+            //     itemPlaced[i] = false;
+            // }
         }
     }
         
     public List<List<Integer>> permute(int[] nums) {
         this.res = new ArrayList<>();
         this.nums = nums;
-        permutation(0, new boolean[nums.length], new ArrayList<>());
+        permutation(0, new ArrayList<>());
         return res;
     }
 }
