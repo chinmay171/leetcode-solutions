ecf9bf0ca0dea078279cc0249788d4525b6520ed
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-10 22:23:36 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -48,24 +48,37 @@ class gfg
 class Solution 
 { 
     //Function to return max value that can be put in knapsack of capacity W.
-    static int memo(int cap, int item, int[] wt, int[] cost, int[][] dp){
-        if(item == cost.length)return 0;
-        if(dp[cap][item] != -1)return dp[cap][item];
-        int yes = (cap >= wt[item]) ? memo(cap-wt[item], item+1, wt, cost, dp)+cost[item] : -1;
-        int no = memo(cap, item +1, wt, cost, dp); 
+    // static int memo(int cap, int item, int[] wt, int[] cost, int[][] dp){
+    //     if(item == cost.length)return 0;
+    //     if(dp[cap][item] != -1)return dp[cap][item];
+    //     int yes = (cap >= wt[item]) ? memo(cap-wt[item], item+1, wt, cost, dp)+cost[item] : -1;
+    //     int no = memo(cap, item +1, wt, cost, dp); 
         
-        return dp[cap][item] = Math.max(yes, no);
-    }
-    static int knapSack(int Capacity, int wt[], int val[], int n) 
+    //     return dp[cap][item] = Math.max(yes, no);
+    // }
+    static int knapSack(int caps, int wt[], int cost[], int n) 
     { 
          // your code here 
-         int[][] dp = new int[Capacity+1][val.length];
-         for(int i = 0; i<=Capacity; ++i){
-             for(int j = 0; j<val.length; ++j){
-                 dp[i][j] = -1;
-             }
-         }
-         return memo(Capacity, 0, wt, val, dp);
+        //  int[][] dp = new int[Capacity+1][val.length];
+        //  for(int i = 0; i<=Capacity; ++i){
+        //      for(int j = 0; j<val.length; ++j){
+        //          dp[i][j] = -1;
+        //      }
+        //  }
+        //  return memo(Capacity, 0, wt, val, dp);
+        
+        int[][] dp = new int[caps+1][cost.length+1];
+        
+        for(int item = 1; item <= cost.length; ++item){
+            for(int cap = 1; cap<= caps; ++cap){
+                int no = dp[cap][item-1];
+                int yes = (cap >= wt[item-1])
+                            ? cost[item-1] + dp[cap-wt[item-1]][item-1]
+                            : -1;
+                dp[cap][item] = Math.max(yes, no);
+            }
+        }
+        return dp[caps][cost.length];
     } 
 }
 
