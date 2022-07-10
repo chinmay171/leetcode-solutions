afb6c8f5042eba49a5650eb2a6901b513cef72bb
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-10 12:23:58 +0530

    Time: 312 ms (5.01%), Space: 51.6 MB (18.49%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int memo(int amount, int lastCoin, int[] coins, int[][] dp){
+        if(amount < 0)return 0;
+        if(amount == 0) return 1;
+        if(dp[amount][lastCoin] != -1)return dp[amount][lastCoin];
+        int ans = 0;
+        for(int i = lastCoin; i<coins.length; ++i){
+            ans += memo(amount - coins[i], i, coins, dp);
+        }
+        return dp[amount][lastCoin] = ans;
+    }
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[amount+1][coins.length+1];
+        for(int i =0; i<=amount; ++i){
+            for(int j = 0; j<=coins.length; ++j){
+                dp[i][j] = -1;
+            }
+        }
+        return memo(amount, 0, coins, dp);
+    }
+}
