20b3855c78d8b95c5a752330c11161171c141f52
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-15 19:48:19 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -48,20 +48,22 @@ class Solution{
 	   // }
 	   // return memo(0, arr, sum, dp);
 	   
-	   int[][] dp = new int[n+1][sum+1];
+	   int[] dp = new int[sum+1];
 	   
 	   
-	   dp[0][0] = 1;
+	   dp[0] = 1;
 	   
 	   for(int i = 1; i<= n; ++i){
+	       int[] newdp = new int[sum+1];
 	       for(int j = 0; j<= sum; ++j){
-	           int no  = dp[i-1][j];
-	           int yes = (j >= arr[i-1]) ? dp[i-1][j-arr[i-1]] : 0;
+	           int no  = dp[j];
+	           int yes = (j >= arr[i-1]) ? dp[j-arr[i-1]] : 0;
 	           
-	           dp[i][j] = (yes + no)% 1000000007;
+	           newdp[j] = (yes + no)% 1000000007;
 	       }
+	       dp = newdp;
 	   }
 	   
-	   return dp[n][sum];
+	   return dp[sum];
 	} 
 }
