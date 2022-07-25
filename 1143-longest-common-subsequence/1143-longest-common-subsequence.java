8788a5c671b824d97564f5dd5f74a2cba3906be7
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-25 11:47:10 +0530

    Time: 29 ms (23.73%), Space: 46.6 MB (27.48%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,24 +1,34 @@
 class Solution {
-    public int lcs(String first, String second, int i, int j,int[][] dp){
-        if(i == first.length() || j == second.length()){
-            return 0;
-        }
-        if(dp[i][j] != -1)return dp[i][j];
-        int ans = 0;
-        if(first.charAt(i) == second.charAt(j)){
-            ans = lcs(first, second, i+1, j+1,dp) + 1;
-        }else{
-            ans = Math.max(lcs(first, second, i+1, j,dp),lcs(first, second, i, j+1,dp));
-        }
-        return dp[i][j] = ans;
-    }
+    // public int lcs(String first, String second, int i, int j,int[][] dp){
+    //     if(i == first.length() || j == second.length()){
+    //         return 0;
+    //     }
+    //     if(dp[i][j] != -1)return dp[i][j];
+    //     int ans = 0;
+    //     if(first.charAt(i) == second.charAt(j)){
+    //         ans = lcs(first, second, i+1, j+1,dp) + 1;
+    //     }else{
+    //         ans = Math.max(lcs(first, second, i+1, j,dp),lcs(first, second, i, j+1,dp));
+    //     }
+    //     return dp[i][j] = ans;
+    // }
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-        for(int i = 0; i<text1.length(); ++i){
-            for(int j = 0; j<text2.length(); ++j){
-                dp[i][j] = -1;
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        // for(int i = 0; i<text1.length(); ++i){
+        //     for(int j = 0; j<text2.length(); ++j){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return lcs(text1, text2, 0, 0,dp);
+        for(int i = text1.length()-1; i>=0; --i){
+            for(int j = text2.length()-1; j>=0; --j){
+                if(text1.charAt(i) == text2.charAt(j)){
+                    dp[i][j] = dp[i+1][j+1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i+1][j],dp[i][j+1]);
+                }
             }
         }
-        return lcs(text1, text2, 0, 0,dp);
+        return dp[0][0];
     }
 }
