466e0270b80f6c3abcaba5a61862accc3b4ef5cb
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-27 21:40:57 +0530

    Time: 29 ms (75.03%), Space: 54.3 MB (21.46%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,37 +1,5 @@
 class Solution {
-//     public boolean memo(int i,int j,String s,String p, Boolean[][] dp){
-//         if(i == s.length() && j == p.length())return true;
-        
-//         if(i < s.length() && j == p.length())return false;
-        
-//         if(i == s.length() && j < p.length()){
-//             for(int k = j; k<p.length(); ++k){
-//                 if(p.charAt(k) != '*')
-//                     return false;
-//             }
-//             return true;
-//         }
-        
-//         if(dp[i][j] != null)return dp[i][j];
-        
-//         char c1= s.charAt(i);
-//         char c2= s.charAt(j);
-        
-//         if(c1 == c2 || c2 == '?'){
-//             return dp[i][j] = memo(i+1, j+1, s, p, dp);
-//         }
-//         if(c2 == '*'){
-            
-//             boolean no = memo(i, j+1, s, p, dp);
-//             if(no == true)return dp[i][j] = true;
-            
-//             boolean yes = memo(i+1, j, s, p, dp);
-//             return dp[i][j] = yes;
-//         }
-        
-//         return dp[i][j] = false;
-//     }
-    public boolean memo(int i, int j, String s1, String s2, Boolean[][] dp) {
+     public boolean memo(int i, int j, String s1, String s2, Boolean[][] dp) {
         if (i == s1.length() && j == s2.length()) {
             return true;
         }
@@ -60,7 +28,7 @@ class Solution {
             return dp[i][j] = memo(i + 1, j + 1, s1, s2, dp);
         }
 
-        if (ch2 == '*') {
+        else if(ch2 == '*') {
             boolean no = memo(i, j + 1, s1, s2, dp);
             if (no == true)
                 return dp[i][j] = true;
@@ -73,7 +41,7 @@ class Solution {
         return dp[i][j] = false;
     }
     public boolean isMatch(String s, String p) {
-        Boolean[][] dp = new Boolean[s.length()+1][p.length()+1];
+        Boolean[][] dp = new Boolean[s.length() + 1][p.length() + 1];
         return memo(0, 0, s, p, dp);
     }
 }
