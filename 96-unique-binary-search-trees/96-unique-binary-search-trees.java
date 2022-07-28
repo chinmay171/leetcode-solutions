e407bc85b702cb3b8d4717f40d041557e109f34f
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-28 13:22:27 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (70.22%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,15 +1,22 @@
 class Solution {
-    public int memo(int n,int[] dp){
-        if(n == 0 || n == 1)return 1;
-        if(dp[n] != 0)return dp[n];
-        int ans = 0;
-        for(int i = 0; i<n; ++i){
-            ans = ans + memo(i, dp) *  memo(n-1 -i, dp);
-        }
-        return dp[n] = ans;
-    }
+    // public int memo(int n,int[] dp){
+    //     if(n == 0 || n == 1)return 1;
+    //     if(dp[n] != 0)return dp[n];
+    //     int ans = 0;
+    //     for(int i = 0; i<n; ++i){
+    //         ans = ans + memo(i, dp) *  memo(n-1 -i, dp);
+    //     }
+    //     return dp[n] = ans;
+    // }
     public int numTrees(int n) {
+        // return memo(n, dp);
         int[] dp = new int[n+1];
-        return memo(n, dp);
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i<=n; ++i){
+            for(int j = 0; j<i; ++j){
+                dp[i] += dp[j] * dp[i-j-1]; 
+            }
+        }
+        return dp[n];
     }
 }
