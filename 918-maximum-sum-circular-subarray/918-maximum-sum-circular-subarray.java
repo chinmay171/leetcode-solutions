8f6d5f44fd6171c677ca775587a58b71e68975e6
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-29 17:07:01 +0530

    Time: 4 ms (86.89%), Space: 64.9 MB (21.83%) - LeetHub

---
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        int total = 0;
+        for(int i = 0;i<nums.length; ++i){
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            total += nums[i];
+            maxSum = Math.max(maxSum, currSum);
+        }
+        
+        int minSum = Integer.MAX_VALUE;
+        currSum = 0;
+        for(int i = 0; i<nums.length; ++i){
+            currSum = Math.min(nums[i], nums[i]+currSum);
+            minSum = Math.min(currSum, minSum);
+        }
+        
+        if(maxSum > 0)
+            return Math.max(maxSum, total-minSum);
+        return maxSum;
+    }
+}
