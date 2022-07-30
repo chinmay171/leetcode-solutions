bf7c3de72c11557707a7d210e6ab6a413fd6c518
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-07-30 14:21:17 +0530

    Time: 1 ms (86.30%), Space: 41.6 MB (49.46%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.java
+++ 486-predict-the-winner/486-predict-the-winner.java
@@ -1,16 +1,26 @@
 class Solution {
-    public int memo(int[] nums, int i, int j){
+    public int memo(int[] nums, int i, int j, int[][] dp){
         if(i > j)return 0;
         if(i == j)return nums[i];
         
-        int c1 = memo(nums, i+2, j);
-        int c2 = memo(nums, i+1, j-1);
-        int c3 = memo(nums, i, j-2);
+        if(dp[i][j] != -1)return dp[i][j];
         
-        return Math.max(nums[i] + Math.min(c1, c2), nums[j] + Math.min(c2, c3));
+        int c1 = memo(nums, i+2, j, dp);
+        int c2 = memo(nums, i+1, j-1, dp);
+        int c3 = memo(nums, i, j-2, dp);
+        
+        return dp[i][j] = Math.max(nums[i] + Math.min(c1, c2),
+                        nums[j] + Math.min(c2, c3));
     }
     public boolean PredictTheWinner(int[] nums) {
-        int aScore = memo(nums, 0, nums.length-1);
+        int[][] dp = new int[nums.length][nums.length];
+        for(int i = 0; i<dp.length; ++i){
+            for(int j = 0; j<dp[0].length; ++j){
+                dp[i][j] = -1;
+            }
+        }
+        
+        int aScore = memo(nums, 0, nums.length-1, dp);
         
         int total = 0;
         for(int i = 0; i<nums.length; ++i){
