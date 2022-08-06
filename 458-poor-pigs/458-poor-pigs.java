d3f72f39ba0e956e18f37d3c54faf9389aebcd7a
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-06 23:20:22 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (81.60%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int pig = 0;
+        while(Math.pow(minutesToTest/minutesToDie + 1, pig) < buckets){
+            pig++;
+        }
+        return pig;
+    }
+}
