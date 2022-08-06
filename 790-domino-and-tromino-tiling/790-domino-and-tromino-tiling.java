07a753cf037f656940e75981b870bf5eb7c96f55
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-06 14:13:48 +0530

    Time: 1 ms (89.35%), Space: 40.6 MB (80.29%) - LeetHub

---
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int m = 1000000007;
+    public int numTilings(int n) {
+        if(n == 1)return 1;
+        if(n == 2)return 2;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i<=n; ++i){
+            dp[i] = ((2 * dp[i-1])% m + dp[i-3])% m;
+        }
+        return dp[n];
+    }
+}
