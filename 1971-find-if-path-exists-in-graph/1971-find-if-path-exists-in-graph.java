5a552d665bb0fecf15b022d5b3663abc483395f4
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-10 12:51:18 +0530

    Time: 126 ms (72.61%), Space: 201.3 MB (47.77%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -13,17 +13,45 @@ class Graph{
         adj[src].add(dest);
         adj[dest].add(src);
     }
-}
-class Solution {
-    public boolean dfs(int source, int destination, Graph g, boolean[] vis){
+    
+    public boolean dfs(int source, int destination, boolean[] vis){
         if(source == destination)return true;
         if(vis[source] == true)return false;
         vis[source] = true;
-        for(int nbr : g.adj[source]){
-            if(dfs(nbr, destination, g, vis) == true)return true;
+        for(int nbr : adj[source]){
+            if(dfs(nbr, destination, vis) == true)return true;
         }
         return false;
     }
+    
+    public boolean dfs(int src, int dest){
+        boolean[] vis = new boolean[adj.length];
+        return dfs(src, dest, vis);
+    }
+    
+    public boolean bfs(int src, int dest){
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] vis = new boolean[adj.length];
+        
+        q.add(src);
+        while(q.size() > 0){
+            int front = q.remove();
+            if(vis[front] == true)continue;
+            if(front == dest) return true;
+            vis[front] = true;
+            
+            for(Integer nbr : adj[front]){
+                if(vis[nbr] == false){
+                    q.add(nbr);
+                }
+            }
+        }
+        return false;
+    }
+}
+class Solution {
+    
     
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         Graph g = new Graph(n);
@@ -32,7 +60,6 @@ class Solution {
             g.addEdge(edges[i][0], edges[i][1]);
         }
         
-        boolean[] vis = new boolean[n];
-        return dfs(source, destination, g, vis);
+        return g.bfs(source, destination);
     }
 }
