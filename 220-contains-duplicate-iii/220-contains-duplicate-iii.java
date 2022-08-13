c19750428af58d00135267c9ad36866f012eafd0
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-13 16:31:23 +0530

    Time: 93 ms (13.62%), Space: 55.4 MB (10.40%) - LeetHub

---
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeSet<Long> vis = new TreeSet<>();
+        
+        for(int i= 0; i< nums.length; ++i){
+            if(i - k - 1 >= 0){
+                vis.remove((long)nums[i-k-1]);
+            }
+            
+            Long floor = vis.floor((long)nums[i]);
+            Long ceil = vis.ceiling((long)nums[i]);
+            
+            if(floor != null && (long)nums[i] - floor <= t){
+                return true;
+            }
+            if(ceil != null && ceil - (long)nums[i] <= t){
+                return true;
+            }
+            
+            vis.add((long)nums[i]);
+        }
+        return false;
+    }
+}
