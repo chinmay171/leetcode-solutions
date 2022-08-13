35676add3d7df3c7651bebe55c97bcb5f77ae04a
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-13 15:33:32 +0530

    Time: 2 ms (99.00%), Space: 43.7 MB (94.99%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length; 
+        
+        for(int i = 0; i < n; ++i){
+            int val = nums[i] % (n+1);
+            nums[val-1] += (n+1);
+        }
+        
+        int missing = 0;
+        int repeating = 0;
+        
+        for(int i = 0; i < n; ++i){
+            if(nums[i] / (n+1) == 0)missing = (i+1);
+            if(nums[i] / (n+1) > 1)repeating = (i+1);
+        }
+        
+        return new int[]{repeating, missing};
+    }
+}
