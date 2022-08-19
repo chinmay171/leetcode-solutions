b7067690a6e1d71670c286e147fa1b94743ce086
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-19 16:43:19 +0530

    Time: 3 ms (63.66%), Space: 57.5 MB (6.73%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = Integer.MIN_VALUE;
+        int freq = 0;
+        for(int val : nums){
+            if(freq <= 0)majority = val;
+            if(val == majority) freq++;
+            else freq--;
+        }
+        
+        return majority;
+    }
+}
