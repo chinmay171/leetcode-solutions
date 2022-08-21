6f410418be6efcbac43b903dca7d4ead40a0bba0
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-21 23:50:28 +0530

    Time: 1 ms (94.34%), Space: 42.1 MB (70.00%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,18 +1,37 @@
 class Solution {
     public int maxProfit(int[] prices) {
+//         if(prices.length == 1)return 0;
+//         int[][] dp = new int[prices.length][2];
+//         // dp[0][0] = -prices[0];
+//         // dp[0][1] = 0;
+//         // dp[1][0] = Math.max(-prices[0], -prices[1]);
+//         // dp[1][1] = Math.max(0, -prices[0] + prices[1]); 
+        
+//         for(int i = 2; i < prices.length; ++i){
+//             dp[i][0] = Math.max(dp[i-1][0], dp[i-2][1] - prices[i]);
+//             dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
+//         }
+        
+//         return dp[prices.length-1][1];
+        
+        
         if(prices.length <= 1)return 0;
-        int[] buy = new int[prices.length];
-        int[] sell = new int[prices.length];
         
-        buy[0] = -prices[0];
-        sell[0] = 0;
-        buy[1] = Math.max(buy[0], sell[0] - prices[1]);
-        sell[1] = Math.max(sell[0], buy[0] + prices[1]);
+        int buy0 = -prices[0];
+        int sell0 = 0;
+        int buy1 = Math.max(-prices[0], -prices[1]);
+        int sell1 = Math.max(0, prices[1] - prices[0]);
         
-        for(int i = 2; i<prices.length; ++i){
-            buy[i] = Math.max(buy[i-1], sell[i-2] - prices[i]);
-            sell[i] = Math.max(sell[i-1], buy[i-1] + prices[i]);
+        for(int i = 2; i < prices.length; ++i){
+            int newBuy = Math.max(buy1, sell0 - prices[i]);
+            int newSell = Math.max(sell1, buy1 + prices[i]);
+            
+            buy0 = buy1;
+            sell0 = sell1;
+            buy1 = newBuy;
+            sell1 = newSell;
         }
-        return sell[prices.length-1];
+        
+        return sell1;
     }
 }
