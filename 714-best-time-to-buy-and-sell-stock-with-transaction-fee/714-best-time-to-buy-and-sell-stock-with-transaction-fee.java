b617acbd24f829889676e078bf02c9ba61afc40f
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-21 23:05:38 +0530

    Time: 4 ms (92.65%), Space: 75 MB (50.12%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,12 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int[][] dp = new int[prices.length+1][2];
-        dp[0][0] = 0 - prices[0];
-        dp[0][1] = 0;
-        for(int i = 1; i<prices.length; ++i){
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i] - fee);
+//         int[][] dp = new int[prices.length][2];
+//         dp[0][0] = 0 - prices[0]; // buying col
+//         dp[0][1] = 0; // selling col
+        
+//         for(int i = 1; i < prices.length; ++i){
+//             //making buying column
+//             dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i]);
+//             //making selling column
+//             dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i] - fee);
+//         }
+        
+//         return Math.max(dp[prices.length-1][0], dp[prices.length-1][1]);
+        
+        int buy = -prices[0];
+        int sell = 0;
+        
+        for(int i = 0; i < prices.length; ++i){
+            int newbuy = Math.max(buy, sell - prices[i]);
+            int newsell= Math.max(sell, buy + prices[i] - fee);
+            
+            buy = newbuy;
+            sell = newsell;
         }
-        return dp[prices.length-1][1];
+        
+        return Math.max(buy, sell);
     }
 }
