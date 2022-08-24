4032b77547a038063ee7c2fadc5158fb384cf2fc
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-24 22:57:12 +0530

    Time: 5 ms (56.32%), Space: 43.2 MB (38.00%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,17 +1,34 @@
 class Solution {
-    public int maxProfit(int k, int[] prices) {
-        if(prices.length == 0 || k == 0)return 0;
-        int[][] dp = new int[k+1][prices.length];
+    public int maxProfit(int cap, int[] prices) {
+        if(prices.length == 0 || cap == 0)return 0;
+//         int[][] dp = new int[k+1][prices.length];
         
-        for(int t = 1; t<=k; ++t){
-            for(int i = 0; i<prices.length; ++i){
-                dp[t][i] = (i-1 >= 0) ? dp[t][i-1] : 0;
+//         for(int t = 1; t<=k; ++t){
+//             for(int i = 0; i<prices.length; ++i){
+//                 dp[t][i] = (i-1 >= 0) ? dp[t][i-1] : 0;
                 
-                for(int j = i-1; j>=0; j--){
-                    dp[t][i] = Math.max(dp[t][i], prices[i]- prices[j] + dp[t-1][j]);
+//                 for(int j = i-1; j>=0; j--){
+//                     dp[t][i] = Math.max(dp[t][i], prices[i]- prices[j] + dp[t-1][j]);
+//                 }
+//             }
+//         }
+//         return dp[k][prices.length-1];
+        
+        int[][][] dp = new int[prices.length+1][2][cap+1];
+        
+        for(int i = prices.length-1; i >= 0; --i){
+            for(int buy = 0; buy <= 1; ++buy){
+                for(int k = 1; k <= cap; ++k){
+                    
+                    if(buy == 0){
+                        dp[i][buy][k] = Math.max( -prices[i] + dp[i+1][1][k], 0 + dp[i+1][0][k]);
+                    }else{
+                        dp[i][buy][k] = Math.max( prices[i] + dp[i+1][0][k-1], 0 + dp[i+1][1][k]);
+                    }
                 }
             }
         }
-        return dp[k][prices.length-1];
+        
+        return dp[0][0][cap];
     }
 }
