1daf1171c5032fa5567b358afcaa6b497ce02483
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-24 14:37:50 +0530

    Time: 2 ms (32.13%), Space: 48.3 MB (23.29%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,24 +1,33 @@
 class Solution {
-    public void dfs(int[][] image, int sr, int sc, int startColor, int newColor){
+    int[][] dirc = {{+1, 0},{-1, 0},{0, +1},{0, -1}};
+    public void dfs(int row, int col, int[][] image,int initialColor, int newColor,boolean[][] vis){
+        if(vis[row][col] == true)return;
         
-        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length){
-            return;
+        vis[row][col] = true;
+        image[row][col] = newColor;
+        
+        for(int i = 0; i < 4; ++i){
+            int nRow = row + dirc[i][0];
+            int nCol = col + dirc[i][1];
+            
+            if(nRow < 0 || nCol < 0 || nRow >= image.length || nCol >= image[0].length) continue;
+            
+            if(image[nRow][nCol] != initialColor || image[nRow][nCol] == newColor) continue;
+            
+            dfs(nRow, nCol, image, initialColor, newColor, vis);
         }
-        
-        if(image[sr][sc] != startColor)return;
-        
-        image[sr][sc] = newColor;
-        
-        dfs(image, sr+1, sc, startColor, newColor);
-        dfs(image, sr-1, sc, startColor, newColor);
-        dfs(image, sr, sc+1, startColor, newColor);
-        dfs(image, sr, sc-1, startColor, newColor);
-        
-        
+        return;
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] == color)return image;
-        dfs(image, sr, sc, image[sr][sc], color);
+        // int[][] ans = new int[image.length][image[0].length];
+        // for(int i = 0; i < image.length; ++i)
+        //     for(int j = 0; j < image[0].length; ++j)
+        //         ans[i][j] = 
+        boolean[][] vis = new boolean[image.length][image[0].length];
+        int initialColor = image[sr][sc];
+        
+        dfs(sr, sc, image, initialColor, color, vis);
+        
         return image;
     }
 }
