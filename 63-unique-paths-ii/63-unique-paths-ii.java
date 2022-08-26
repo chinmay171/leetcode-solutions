3fb2b630f53a25e04472928789a429c27e05d10a
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-26 20:59:30 +0530

    Time: 10 ms (5.60%), Space: 42.9 MB (13.29%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,23 +1,22 @@
 class Solution {
-    public int memo(int sr, int sc, int[][] grid, int[][] dp){
-        if(sr > grid.length-1 || sc > grid[0].length-1)return 0;
-        if(sr == grid.length-1 && sc == grid[0].length-1)return 1;
-        if(grid[sr][sc] == 1)return 0;
-        if(dp[sr][sc] != -1)return dp[sr][sc];
+    public int memo(int r, int c, int[][] grid, int[][] dp){
+        if(r == grid.length || c == grid[0].length)return 0;
+        if(grid[r][c] == 1)return 0;
         
-        return dp[sr][sc] = (memo(sr+1, sc, grid, dp) + memo(sr, sc+1, grid, dp));
+        if(r == grid.length - 1 && c == grid[0].length -1)return 1;
+        
+        if(dp[r][c] != 0)return dp[r][c];
+        
+        int ans = 0;
+        ans += memo(r+1, c, grid, dp);
+        ans += memo(r, c+1, grid, dp);
+        
+        return dp[r][c] = ans;
     }
     
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid[0][0] == 1)return 0;
-        if(obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1] == 1)return 0;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
         
-        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-        for(int i = 0; i<obstacleGrid.length; ++i){
-            for(int j = 0; j<obstacleGrid[0].length; ++j){
-                dp[i][j] = -1;
-            }
-        }
-        return memo(0, 0, obstacleGrid, dp);
+        return memo(0, 0, grid, dp);
     }
 }
