61125ae3278148f0e69089a2ed33888b06c9a9d0
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-08-31 14:37:18 +0530

    Time: 8 ms (65.88%), Space: 55.1 MB (44.27%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,44 +1,44 @@
 class Solution {
-    int[][] dir = {{+1, 0},{-1, 0},{0, +1},{0, -1}};
-    
-    public void dfs(int r, int c, int[][] heights, boolean[][] vis){
-        if(vis[r][c] == true)return;
-        
+    int[][] dir = { { +1, 0 }, { -1, 0 }, { 0, +1 }, { 0, -1 } };
+
+    // Time - O(N * M) where N = rows, M = columns: DFS
+    public void DFS(int r, int c, int[][] heights, boolean[][] vis) {
+        if (vis[r][c] == true)
+            return;
+
         vis[r][c] = true;
-        for(int i = 0; i < 4; ++i){
-            int nr = r +dir[i][0];
-            int nc = c +dir[i][1];
-            
-            if( nr < 0 || nc < 0 || nr >= heights.length || nc >= heights[0].length){
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dir[d][0];
+            int nc = c + dir[d][1];
+
+            if (nr < 0 || nc < 0 || nr >= heights.length || nc >= heights[r].length)
                 continue;
-            }
-            if(heights[nr][nc] >= heights[r][c]){
-                dfs(nr, nc, heights, vis);
+
+            if (heights[nr][nc] >= heights[r][c]) {
+                DFS(nr, nc, heights, vis);
             }
         }
-        
     }
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int n = heights.length;
-        int m = heights[0].length;
-        
+        int n = heights.length, m = heights[0].length;
+
         boolean[][] pacific = new boolean[n][m];
-        
-        // For Pacific Ocean
-        for(int i = 0; i < m; ++i) dfs(0, i, heights, pacific);//Top Wall
-        for(int i = 0; i < n; ++i) dfs(i, 0, heights, pacific);//Left Wall
-        
+        for (int i = 0; i < m; i++)
+            DFS(0, i, heights, pacific); // TOP WALL
+        for (int i = 0; i < n; i++)
+            DFS(i, 0, heights, pacific); // LEFT WALL
+
         boolean[][] atlantic = new boolean[n][m];
-        
-        // For Atlantic Ocean
-        for(int i = 0; i < m; ++i) dfs(n-1, i, heights, atlantic);//Bottom Wall
-        for(int i = 0; i < n; ++i) dfs(i, m-1, heights, atlantic);//Right Wall
-        
+        for (int i = 0; i < m; i++)
+            DFS(n - 1, i, heights, atlantic); // BOTTOM WALL
+        for (int i = 0; i < n; i++)
+            DFS(i, m - 1, heights, atlantic); // RIGHT WALL
+
         List<List<Integer>> res = new ArrayList<>();
-        
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < m; ++j){
-                if(pacific[i][j] == true && atlantic[i][j] == true){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (pacific[i][j] == true && atlantic[i][j] == true) {
                     List<Integer> point = new ArrayList<>();
                     point.add(i);
                     point.add(j);
