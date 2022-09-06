6d5e853a63ce007c12aae9ca28e9801176143250
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-09-06 18:46:00 +0530

    Time: 55 ms (78.18%), Space: 42.3 MB (92.58%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
@@ -1,32 +1,35 @@
 class Solution {
     public int minimumMountainRemovals(int[] nums) {
-        int[] left = new int[nums.length];
-        for(int i = 0; i<nums.length; ++i){
-            left[i] = 1;
-            for(int j = 0; j<nums.length; ++j){
+        int n = nums.length; 
+        
+        int[] IncreasingFromLeft = new int[n];
+        for(int i = 0; i < n; ++i){
+            IncreasingFromLeft[i] = 1;
+            for(int j = 0; j < i; ++j){
                 if(nums[i] > nums[j]){
-                    left[i] = Math.max(left[i], left[j] + 1);
+                    IncreasingFromLeft[i] = Math.max(IncreasingFromLeft[i], IncreasingFromLeft[j] + 1);
                 }
             }
         }
         
-        int[] right = new int[nums.length];
-        for(int i = nums.length-1; i>=0; i--){
-            right[i] = 1;
-            for(int j = nums.length-1; j>i; j--){
+        int[] IncreasingFromRight = new int[n];
+        for(int i = n - 1; i >= 0; --i){
+            IncreasingFromRight[i] = 1;
+            for(int j = i+1; j < n; ++j){
                 if(nums[i] > nums[j]){
-                    right[i] = Math.max(right[i], right[j] + 1);
+                    IncreasingFromRight[i] = Math.max(IncreasingFromRight[i], IncreasingFromRight[j] + 1);
                 }
             }
         }
         
-        int maxBitonic = 0;
-        for(int i = 0; i<nums.length; ++i){
-            int curr = left[i] + right[i] - 1;
-            if(left[i] > 1 && right[i] > 1){
-                maxBitonic = Math.max(maxBitonic, curr);
+        int LengthOfMountainArray = 0;
+        for(int i = 0; i < n; ++i){
+            int currLengthOfMountainArray = IncreasingFromLeft[i] + IncreasingFromRight[i] - 1;
+            if(IncreasingFromLeft[i] > 1 && IncreasingFromRight[i] > 1){
+                LengthOfMountainArray = Math.max(currLengthOfMountainArray, LengthOfMountainArray);
             }
         }
-        return nums.length - maxBitonic;
+        
+        return n - LengthOfMountainArray;
     }
 }
