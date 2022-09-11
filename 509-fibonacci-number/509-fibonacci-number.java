a85cf3177f709b2ebc144dbca04e72b6c4fe69dc
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-09-11 18:12:48 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (34.31%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,23 +1,25 @@
 class Solution {
-    // public int fib(int n, int[] dp){
-    //     if(n<=1)return n;
-    //     if(dp[n] != -1){
-    //         return dp[n];
-    //     }
-    //     int prev1 = fib(n-1, dp);
-    //     int prev2 = fib(n-2, dp);
-    //     dp[n] = prev1 + prev2;
-    //     return prev1+prev2;
+    // public int memo(int n, int[] dp){
+    //     if(n == 0)return 0;
+    //     if(n == 1)return 1;
+    //     if(dp[n] != -1)return dp[n];
+    //     return dp[n] = memo(n-1, dp) + memo(n-2, dp);
     // }
     public int fib(int n) {
-        if(n <= 1) return n;
-        int prev1 = 0;
-        int prev2 = 1;
-        for(int i = 2; i <= n; ++i){
+        if(n == 0)return 0;
+        // int[] dp = new int[n+1];
+        // for(int i = 0; i<=n; ++i){
+        //     dp[i] = -1;
+        // }
+        // return memo(n, dp);
+        // int[] dp = new int[n+1];
+        int prev2 = 0;
+        int prev1 = 1;
+        for(int i = 2; i<= n; ++i){
             int curr = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = curr;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return prev2;
+        return prev1;
     }
 }
