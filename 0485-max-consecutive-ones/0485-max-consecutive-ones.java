abc1d1b92927570c221d04236dc3d8aa6d34be6e
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-12-20 23:38:10 +0530

    Time: 5 ms (48.27%), Space: 56.3 MB (64.41%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,19 +1,31 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
         
+//         int max = 0;
+//         for(int i = 0; i < nums.length; ++i){
+//             if(nums[i] == 1){
+//                 map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//                 if(map.get(nums[i]) > max){
+//                     max = map.get(nums[i]);
+//                 }
+//             }else{
+//                 map.put(1, 0);
+//             }
+//         }
+        
+//         return max;
         int max = 0;
         for(int i = 0; i < nums.length; ++i){
-            if(nums[i] == 1){
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-                if(map.get(nums[i]) > max){
-                    max = map.get(nums[i]);
-                }
-            }else{
-                map.put(1, 0);
+            int curr = 0;
+            while(i < nums.length && nums[i] == 1){
+                curr++;
+                i++;
+                max = Math.max(max, curr);
             }
+            
         }
-        
         return max;
+        
     }
 }
