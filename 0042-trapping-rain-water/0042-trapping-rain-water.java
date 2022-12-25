6d0e27d3f32175542dc46de3fcf9cd180934c3f0
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2022-12-26 01:29:43 +0530

    Time: 1 ms (99.59%), Space: 42.8 MB (90.60%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,43 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = 0;
-        int rightMax = 0;
+//         int left = 0;
+//         int right = height.length - 1;
+//         int leftMax = 0;
+//         int rightMax = 0;
+//         int res = 0;
+        
+//         while(left <= right){
+//             if(height[left] <= height[right]){
+//                 if(height[left] >= leftMax) leftMax = height[left];
+//                 else res += (leftMax - height[left]);
+//                 left++;
+//             }else{
+//                 if(height[right] >= rightMax) rightMax = height[right];
+//                 else res += (rightMax - height[right]);
+//                 right--;
+//             }
+//         }
+        
+//         return res;
+        int n = height.length;
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        int rightMax = -1;
+        for(int i = 0; i < n; ++i){
+            pre[i] = Math.max(rightMax, height[i]);
+            rightMax = Math.max(rightMax, height[i]);
+        }
+
+        int leftMax = -1;
+        for(int i = n-1; i >= 0; --i){
+            suf[i] = Math.max(leftMax, height[i]);
+            leftMax = Math.max(leftMax, height[i]);
+        }
+        
         int res = 0;
         
-        while(left <= right){
-            if(height[left] <= height[right]){
-                if(height[left] >= leftMax) leftMax = height[left];
-                else res += (leftMax - height[left]);
-                left++;
-            }else{
-                if(height[right] >= rightMax) rightMax = height[right];
-                else res += (rightMax - height[right]);
-                right--;
-            }
+        for(int i = 0; i < n; ++i){
+            res += (Math.min(pre[i], suf[i]) - height[i]);
         }
         
         return res;
