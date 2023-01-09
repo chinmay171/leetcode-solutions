75387541a78bac6a83354700b76ac4a38e747c56
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-01-09 23:52:18 +0530

    Time: 1 ms (100.00%), Space: 41.2 MB (87.47%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] freq = new int[26];
+        boolean[] vis = new boolean[26];
+        
+        char[] chs = s.toCharArray();
+        for(char ch : chs) freq[ch-'a']++;
+        
+        StringBuilder str = new StringBuilder();
+        for(char ch : chs){
+            freq[ch-'a']--;
+            if(vis[ch-'a'] == true) continue;
+            vis[ch-'a'] = true;
+            
+            while(str.length() > 0){
+                char top = str.charAt(str.length() - 1);
+                if(top > ch && freq[top-'a'] > 0){
+                    str.deleteCharAt(str.length() - 1);
+                    vis[top-'a'] = false;
+                }else break;
+            }
+            
+            str.append(ch);
+        }
+        
+        return str.toString();
+    }
+}
