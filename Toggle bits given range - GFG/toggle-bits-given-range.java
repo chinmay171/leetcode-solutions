e174eaa8dd5e872190f80a4dd3626edbf5a73319
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-01-15 16:38:22 +0530

    Added solution - LeetHub

---
+++ Toggle bits given range - GFG/toggle-bits-given-range.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int N = Integer.parseInt(S[0]);
+            int L = Integer.parseInt(S[1]);
+            int R = Integer.parseInt(S[2]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.toggleBits(N,L,R));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int toggleBits(int N , int L , int R) {
+        // code here
+        for(int i = L-1; i <= R-1; ++i){
+            int mask = 1 << i;
+            N = mask ^ N;
+        }
+        
+        return N;
+    }
+};
