9164ceac19a36623ba0fd07bb7db71644b5debca
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-01-22 12:30:46 +0530

    Time: 1 ms (98.96%), Space: 50.4 MB (39.38%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,17 +1,14 @@
 class Solution {
+    public void recursion(char[] s, int left, int right){
+        if(left >= right) return;
+        
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        
+        recursion(s, left+1, right-1);
+    }
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length - 1;
-        
-        while(left < right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            
-            left++;
-            right--;
-        }
-        
-        return;
+        recursion(s, 0, s.length-1);
     }
 }
