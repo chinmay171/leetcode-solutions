517c3f8de4a4fc45e744c7c9fd67a4369490790c
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-03-05 11:41:11 +0530

    Time: 1 ms (26.39%), Space: 40.9 MB (19.12%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,18 +14,56 @@
  * }
  */
 class Solution {
-    List<Integer> postorder = new ArrayList<>();
+//     List<Integer> postorder = new ArrayList<>();
     
-    public void helper(TreeNode root){
-        if(root == null) return;
+//     public void helper(TreeNode root){
+//         if(root == null) return;
         
-        helper(root.left);
-        helper(root.right);
-        postorder.add(root.val);
+//         helper(root.left);
+//         helper(root.right);
+//         postorder.add(root.val);
+//     }
+    
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         helper(root);
+//         return postorder;
+//     }
+    class pair{
+        TreeNode root;
+        int state = 1;
+        
+        pair(TreeNode root){
+            this.root = root;
+        }
     }
     
-    public List<Integer> postorderTraversal(TreeNode root) {
-        helper(root);
+    public List<Integer> postorderTraversal(TreeNode root){
+        List<Integer> preorder = new ArrayList<>();
+        List<Integer> inorder = new ArrayList<>();
+        List<Integer> postorder = new ArrayList<>();
+        Stack<pair> stk = new Stack<>();
+        if(root != null) stk.push(new pair(root));
+        
+        while(stk.size() > 0){
+            pair front = stk.peek();
+            if(front.state == 1){
+                preorder.add(front.root.val);
+                front.state++;
+                if(front.root.left != null){
+                    stk.push(new pair(front.root.left));
+                }
+            }else if(front.state == 2){
+                inorder.add(front.root.val);
+                front.state++;
+                if(front.root.right != null){
+                    stk.push(new pair(front.root.right));
+                }
+            }else{
+                postorder.add(front.root.val);
+                stk.pop();
+            }
+        }
+        
         return postorder;
     }
 }
