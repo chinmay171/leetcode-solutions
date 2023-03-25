1ffdae9e8b44f58073b2d438c254d31073afc1e7
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-03-25 20:25:36 +0530

    Time: 2 ms (28.45%), Space: 42.2 MB (43.95%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public int hieght(TreeNode root){
+        if(root == null) return 0;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        
+        int ht = 0;
+        while(q.size() > 0){
+            ht++;
+            for(int i = q.size(); i > 0; i--){
+                TreeNode top = q.remove();
+                
+                if(top.left != null) q.add(top.left);
+                if(top.right != null) q.add(top.right);
+            }
+        }
+        
+        return ht;
+    }
+    
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        if(root == null) return null;
+        if(root.left == null && root.right == null) return root;
+        
+        int htLeft = -1, htRight = -1;
+        if(root.left != null) htLeft = hieght(root.left);
+        if(root.right != null) htRight = hieght(root.right);
+        
+        if(htLeft == htRight) return root;
+        else if(htLeft > htRight) return lcaDeepestLeaves(root.left);
+        else return lcaDeepestLeaves(root.right);
+    }
+}
