0c1db32dd6a004283d2ea22985eeb1df9ce7f5c7
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-03-25 22:33:47 +0530

    Added solution - LeetHub

---
+++ Min distance between two given nodes of a Binary Tree - GFG/min-distance-between-two-given-nodes-of-a-binary-tree.java
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Main {
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String s = br.readLine().trim();
+            Node root = buildTree(s);
+            String[] ab = br.readLine().trim().split(" ");
+            int a = Integer.parseInt(ab[0]);
+            int b = Integer.parseInt(ab[1]);
+
+            GfG g = new GfG();
+            System.out.println(g.findDist(root, a, b));
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+
+/* Should return minimum distance between a and b
+   in a tree with given root*/
+class GfG {
+    public static Node findLCA(Node root, int a, int b){
+        if(root == null || root.data == a || root.data == b) return root;
+        
+        Node left = findLCA(root.left, a, b);
+        Node right  = findLCA(root.right, a, b);
+        
+        if(left != null && right != null) return root;
+        else if(left != null && right == null) return left;
+        else if(left == null && right != null) return right;
+        return null;
+    }
+    
+    public static int dfs(Node root, int t){
+        if(root == null) return 0;
+        if(root.data == t) return 1;
+        
+        int left = dfs(root.left, t);
+        int right = dfs(root.right, t);
+        
+        if(left != 0) return left + 1;
+        if(right != 0) return right + 1;
+        return left + right;
+}
+    
+    int findDist(Node root, int a, int b) {
+        Node lca = findLCA(root, a, b);
+        int distA = dfs(lca, a) - 1;
+        int distB = dfs(lca, b) - 1;
+        return distA + distB;
+    }
+}
