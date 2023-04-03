952a8e51d93e16cc9394623e7afbdab6327df32f
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-04-03 23:17:35 +0530

    Time: 17 ms (98.31%), Space: 50.7 MB (59.93%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,22 +1,12 @@
 class Solution {
-    // 1 2 4 5
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        
-        int l = 0;
-        int r = people.length-1;
-        int count = 0;
-        
+        int l = 0, r = people.length-1, count = 0;
         while(l <= r){
-            int currWieght = people[l] + people[r];
-            
-            if(currWieght <= limit){
-                l++;
-            }
+            if(people[l] + people[r] <= limit) l++;
             count++;
             r--;
         }
-        
         return count;
     }
 }
