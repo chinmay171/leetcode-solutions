5337306ddbd82d8e418bd58ae9c1a5d0fb7d7447
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-04-07 13:21:38 +0530

    Time: 2 ms (100.00%), Space: 43.1 MB (14.25%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -13,34 +13,67 @@
  *     }
  * }
  */
-// 1 2 3
 // 3 2 1
+// 2 3 1
 class Solution { 
-    ArrayList<Integer> inorder = new ArrayList<>();
-    int idx = 0;
+//     ArrayList<Integer> inorder = new ArrayList<>();
+//     int idx = 0;
+    
+//     public void dfs(TreeNode root){
+//         if(root == null) return;
+        
+//         dfs(root.left);
+//         inorder.add(root.val);
+//         dfs(root.right);
+//     }
+    
+//     public void dfs1(TreeNode root){
+//         if(root == null) return;
+        
+//         dfs1(root.left);
+//         if(inorder.get(idx) != root.val){
+//             root.val = inorder.get(idx);
+//         }
+//         idx++;
+//         dfs1(root.right);
+        
+//     }
+    TreeNode prev;
+    TreeNode first;
+    TreeNode mid;
+    TreeNode second;
     
     public void dfs(TreeNode root){
         if(root == null) return;
         
         dfs(root.left);
-        inorder.add(root.val);
+        if(prev != null && (root.val < prev.val)){
+            if(first == null){
+                first = prev;
+                mid = root;
+            }else{
+                second = root;
+            }
+        }
+        prev = root;
         dfs(root.right);
     }
-    
-    public void dfs1(TreeNode root){
-        if(root == null) return;
-        
-        dfs1(root.left);
-        if(inorder.get(idx) != root.val){
-            root.val = inorder.get(idx);
-        }
-        idx++;
-        dfs1(root.right);
-        
-    }
     public void recoverTree(TreeNode root) {
+        // dfs(root);
+        // Collections.sort(inorder);
+        // dfs1(root);
+        first = second = mid = null;
+        prev = new TreeNode(Integer.MIN_VALUE);
         dfs(root);
-        Collections.sort(inorder);
-        dfs1(root);
+        
+        if(first != null && second != null){
+            int t = first.val;
+            first.val = second.val;
+            second.val = t;
+        }else if(first != null && mid != null){
+            int t = first.val;
+            first.val = mid.val;
+            mid.val = t;
+        }
     }
 }
