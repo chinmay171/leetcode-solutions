1984f6e97459c26bf9e0e6dc6bec325675cd3c05
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-04-17 00:45:18 +0530

    Time: 48 ms (81.79%), Space: 62.2 MB (96.39%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,47 +1,31 @@
 class Graph{
     ArrayList<Integer> adj[];
-    
     Graph(int n){
         adj = new ArrayList[n];
-        for(int i = 0; i < n; ++i){
+        for(int i = 0; i < n; ++i)
             adj[i] = new ArrayList<>();
-        }
     }
-    
     public void addEdge(int src, int dest){
         adj[src].add(dest);
         adj[dest].add(-src);
     }
 }
-
 class Solution {
-    public int edgesChanged = 0;
-    
+    int ans = 0;
     public void dfs(int src, Graph g, boolean[] vis){
-        if(vis[Math.abs(src)] == true) return;
         vis[src] = true;
-        
-        for(int n : g.adj[src]){
-            if(vis[Math.abs(n)] == true) continue;
-            if(n > 0){
-                edgesChanged++;
-                dfs(n, g, vis);
-            }else{
-                dfs(-n, g, vis);
-            }
+        for(int nbr : g.adj[src]){
+            if(vis[Math.abs(nbr)] == true) continue;
+            dfs(Math.abs(nbr), g, vis);
+            if(nbr > 0) ans++;
         }
     }
-    
     public int minReorder(int n, int[][] connections) {
         Graph g = new Graph(n+1);
-        
-        for(int[] connection : connections){
+        for(int[] connection : connections)
             g.addEdge(connection[0]+1, connection[1]+1);
-        }
-        
-        boolean[] visited = new boolean[n+1];
-        
-        dfs(1, g, visited);
-        return edgesChanged;
+        boolean[] vis = new boolean[n+1];
+        dfs(1, g, vis);
+        return ans;
     }
 }
