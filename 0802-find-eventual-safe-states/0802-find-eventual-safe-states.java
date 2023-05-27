eeebb0f91dd0875d96a1269aaedfbb9708b8037b
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-05-27 13:09:20 +0530

    Time: 5 ms (98.63%), Space: 55.2 MB (6.19%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean dfs(int src, int[][] graph, int[] vis){
+        if(vis[src] == -1) return true;
+        if(vis[src] == 1) return false;
+        vis[src] = -1;
+        
+        for(int nbr : graph[src])
+            if(dfs(nbr, graph, vis) == true) return true;
+    
+        vis[src] = 1;
+        return false;
+    } 
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int[] vis = new int[graph.length];
+        
+        for(int i = 0; i < graph.length; ++i)
+            if(vis[i] == 0) dfs(i, graph, vis);
+        
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < graph.length; ++i){
+            if(vis[i] == 1) ans.add(i);
+        }
+        
+        return ans;
+    }
+}
