1b566420cf4c2980022534ff46de1f5b30f2ceab
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-09-25 22:20:00 +0530

    Time: 1786 ms (20.06%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
 SELECT query_name,
 ROUND(AVG(rating / position), 2) AS quality,
-CASE WHEN SUM(q.rating) IS NULL THEN 0
+CASE WHEN SUM(q.rating < 3) IS NULL THEN 0
      ELSE ROUND(SUM(q.rating < 3) /
             COUNT(q.query_name) * 100, 2)
 END AS poor_query_percentage
