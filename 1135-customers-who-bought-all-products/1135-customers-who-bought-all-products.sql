3f315f23af9975780db82c1d2721cd31dc987b93
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-09-26 10:25:35 +0530

    Time: 1313 ms (29.89%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,11 +1,5 @@
-# Write your MySQL query statement below
-# SELECT a.customer_id FROM(
-
 SELECT customer_id
 FROM Customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key)
        = (SELECT COUNT(*) FROM Product);
-
-# ) a
-# WHERE a.freq = (SELECT COUNT(*) FROM Product);
