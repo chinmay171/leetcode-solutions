aa19907902507c5ebfd86715e688441f0cdda846
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-10-07 13:22:22 +0530

    Time: 3 ms (43.87%), Space: 42.7 MB (98.16%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,26 +14,29 @@
  * }
  */
 class Solution {
-    public int findIndex(int target, int[] array){
-        for(int i = 0; i < array.length; i++) if(array[i] == target) return i; 
+    public int findIdx(int[] in, int l, int r, int x){
+        for(int i = l; i <= r; i++)
+            if(in[i] == x) return i;
         return -1;
     }
-    public TreeNode dfs(int[] preorder, int[] inorder, int pl, int pr, int il, int ir){
-        if(pl > pr || il > ir) return null;
-        
-        TreeNode temp = new TreeNode(preorder[pl]);
-        int idxAtInorder = findIndex(preorder[pl], inorder);
-        
-        int lengthOfLeft = idxAtInorder - il;
-        int lengthOfRight = ir - idxAtInorder;
-        
-        temp.left = dfs(preorder, inorder, pl+1, pl+lengthOfLeft, il, idxAtInorder-1);
-        temp.right = dfs(preorder, inorder, pl+lengthOfLeft+1, pr, idxAtInorder+1, ir);
-        
-        return temp;
+
+    public TreeNode rec(int[] preorder, int pl, int pr, int[] inorder, int il, int ir){
+        // System.out.println(pl + " " + pr);
+        // if(pr == pl || ir == il) return  new TreeNode(preorder[pr]);
+        if(pr < pl || ir < il) return null;
+
+        TreeNode curr = new TreeNode(preorder[pl]);
+        int idxAtInorder = findIdx(inorder, il, ir, preorder[pl]);
+        int leftLen = idxAtInorder -  il;
+        int rightLen = ir - idxAtInorder;
+ 
+        curr.left = rec(preorder, pl + 1, pl + leftLen, inorder, il, idxAtInorder - 1);
+        curr.right = rec(preorder, pr - rightLen + 1, pr, inorder, idxAtInorder + 1, ir);
+
+        return curr;
     }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int n = preorder.length;
-        return dfs(preorder, inorder, 0, n-1, 0, n-1);
+        return rec(preorder, 0, n-1, inorder, 0, n-1);
     }
 }
