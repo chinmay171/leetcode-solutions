3ba58747ab87028131738b0ea6e88705fbc5a6f9
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-10-13 19:17:24 +0530

    Time: 17 ms (62.4%), Space: 46.6 MB (36.61%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -14,33 +14,55 @@
  * }
  */
 class BSTIterator {
-    ArrayList<Integer> ans;
-    int i;
-
-    public void dfs(TreeNode root){
-        if(root == null) return;
-
-        dfs(root.left);
-        ans.add(root.val);
-        dfs(root.right);
-    }
-
+    Stack<TreeNode> stk; 
     public BSTIterator(TreeNode root) {
-        ans = new ArrayList<>();
-        ans.add(-1);
-        dfs(root);
-        i = 0;
+        stk = new Stack<>();
+        inorderSucc(root);
+    }
+    
+    public void inorderSucc(TreeNode curr){
+        while(curr != null){
+            stk.push(curr);
+            curr = curr.left;
+        }
     }
     
     public int next() {
-        i++;
-        return ans.get(i);
+        TreeNode curr = stk.pop();
+        inorderSucc(curr.right);
+        return curr.val;
     }
     
     public boolean hasNext() {
-        if(i < ans.size()-1) return true;
-        return false;
+        return (stk.size() > 0);
     }
+    // ArrayList<Integer> ans;
+    // int i;
+
+    // public void dfs(TreeNode root){
+    //     if(root == null) return;
+
+    //     dfs(root.left);
+    //     ans.add(root.val);
+    //     dfs(root.right);
+    // }
+
+    // public BSTIterator(TreeNode root) {
+    //     ans = new ArrayList<>();
+    //     ans.add(-1);
+    //     dfs(root);
+    //     i = 0;
+    // }
+    
+    // public int next() {
+    //     i++;
+    //     return ans.get(i);
+    // }
+    
+    // public boolean hasNext() {
+    //     if(i < ans.size()-1) return true;
+    //     return false;
+    // }
 }
 
 /**
