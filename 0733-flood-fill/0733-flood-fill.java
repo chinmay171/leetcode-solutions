be1adfec2316d492a25f925183b52721aa2498fa
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-10-23 01:02:50 +0530

    Time: 1 ms (43.93%), Space: 43.9 MB (71.18%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,23 +1,22 @@
 class Solution {
     int[][] dir = {{-1, 0}, {0, +1}, {+1, 0}, {0, -1}};
-    public void dfs(int[][] grid, int r, int c, int toBeColored, int initial, boolean[][] vis){
-        if(vis[r][c] == true) return;
-        vis[r][c] = true;
-        grid[r][c] = toBeColored;
-        
+    public void dfs(int[][] mat, int r, int c, int oldC, int newC){
+        if(mat[r][c] == newC) return;
+        mat[r][c] = newC;
         for(int i = 0; i < 4; ++i){
             int nr = r + dir[i][0];
             int nc = c + dir[i][1];
-            
-            if(nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length)
+
+            if(nr < 0 || nc < 0 || nr >= mat.length || nc >= mat[0].length)
                 continue;
-            if(initial != grid[nr][nc]) continue;
-            dfs(grid, nr, nc, toBeColored, initial, vis);
+            if(mat[nr][nc] != oldC) continue;
+
+            dfs(mat, nr, nc, oldC, newC);
         }
     }
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] vis = new boolean[image.length][image[0].length];
-        dfs(image, sr, sc, color, image[sr][sc], vis);
+        dfs(image, sr, sc, image[sr][sc], color);
         return image;
     }
 }
