917bd7293a5727e61cd13e600546ad5d99ee0030
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-10-27 23:56:03 +0530

    Time: 2 ms (43.33%), Space: 43.7 MB (79.11%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,40 +1,33 @@
 class Solution {
-    HashSet<Integer> one;
-    HashSet<Integer> two;
 
-    public boolean dfs(int src, int[][] graph, int set, int[] vis){
+    Queue<Integer> q;
+    public boolean bfs(int src, int[][] graph, int set, int[] vis){
+        q.add(src);
+        vis[src] = set;
 
-        if(vis[src] == 2) return true;
-        if(vis[src] == 1){
-            if(set == 2 && two.contains(src) == true) return false;
-            if(set == 1 && one.contains(src) == true) return false;
-            return true;
+        while(q.size() > 0){
+            set++;
+            for(int i = q.size(); i > 0; i--){
+                int top = q.remove();
+                for(int nbr : graph[top]){
+                    if(vis[nbr] == 0) vis[nbr] = set;
+                    else if(vis[nbr] % 2 == set % 2) continue;
+                    else return false;
+                    q.add(nbr);
+                }
+            }
         }
-        vis[src] = 1;
-
-        int currSet = -1;
-        if(set == -1 || set == 2) currSet = 1;
-        else if(set == 1) currSet = 2;
-
-        if(currSet == 1) one.add(src);
-        if(currSet == 2) two.add(src);
-
-        for(int nbr : graph[src])
-            if(dfs(nbr, graph, currSet, vis) == false) return false;
-        vis[src] = 2;
 
         return true;
     }
 
     public boolean isBipartite(int[][] graph) {
-        one = new HashSet<>();
-        two = new HashSet<>();
-
-        int[] vis = new int[graph.length];  
+        int[] vis = new int[graph.length];
+        q = new ArrayDeque<>();
 
         for(int i = 0; i < graph.length; ++i)
-            if(vis[i] != 2 && dfs(i, graph, -1, vis) == false) return false;
-
+            if(vis[i] == 0 && bfs(i, graph, 1, vis) == false) return false;
+        
         return true;
     }
 }
