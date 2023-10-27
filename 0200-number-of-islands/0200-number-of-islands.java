2750ec93be8b440063a6d49b5d01785ec8a83418
Chinmay Gupta <72167507+chinmay171@users.noreply.github.com>
2023-10-28 00:43:54 +0530

    Time: 4 ms (43.18%), Space: 47.2 MB (83.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,32 @@
 class Solution {
+
     int[][] dir = {{-1, 0}, {0, +1}, {+1, 0}, {0, -1}};
-    
-    public void dfs(int r, int c, char[][] grid, boolean[][] vis){
-        if(vis[r][c] == true) return;
-        vis[r][c] = true;
-        
-        
+
+    public void dfs(int r, int c, char[][] grid, int[][] vis){
+        vis[r][c] = 1;
+
         for(int i = 0; i < 4; ++i){
             int nr = r + dir[i][0];
             int nc = c + dir[i][1];
-            
-            if(nr < 0 || nc < 0 || nr >= grid.length || nc >= grid[0].length){
+
+            if(nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length){
                 continue;
             }
-            if(grid[nr][nc] == '1')
-                dfs(nr, nc, grid, vis);
+            if(vis[nr][nc] == 0 && grid[nr][nc] == '1') dfs(nr, nc, grid, vis);
         }
     }
-    
+
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        
-        boolean[][] vis = new boolean[n][m];
+        int[][] vis = new int[grid.length][grid[0].length];
+
         int ans = 0;
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < m; ++j){
-                // System.out.print(vis[i][j] + " ");
-                if(vis[i][j] == false && grid[i][j] == '1'){
-                    // System.out.println(i + " " + j);
+        for(int i = 0; i < grid.length; ++i)
+            for(int j = 0; j < grid[0].length; ++j)
+                if(vis[i][j] == 0 && grid[i][j] == '1'){
                     dfs(i, j, grid, vis);
                     ans++;
                 }
-            }
-            // System.out.println();
-        }
+
         return ans;
     }
 }
